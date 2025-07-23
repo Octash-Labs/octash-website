696072661ee3245c3d3c8ce34c19ce283cf886a0
Ayotunde Salako <salakoayotunde@gmail.com>
2025-07-23 12:10:47 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from 'react-dom/client'
+import { initializeEmailJS } from './lib/emailjs';
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+// Initialize EmailJS when app starts
+initializeEmailJS();
